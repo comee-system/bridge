7f50aa015735d95429398288babb51e844f2062d
comee-system <system@comee.jp>
2021-04-25 01:14:57 +0900

    微修正

--- cakeapp/src/Controller/Admin/UsersController.php
+++ cakeapp/src/Controller/Admin/UsersController.php
@@ -74,7 +74,7 @@ class UsersController extends AppController
     {
         $user = $this->Users->newEntity();
         if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
+            $user = $this->Users->patchEntity($user, $this->request->getData(),['validate'=>false]);
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
 
